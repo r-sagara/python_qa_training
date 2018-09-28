319f6014d43d6690059f6905bb0058c3c8f73f46
Sagara <heshagrade@gmail.com>
2018-09-28 23:15:26 +0300

    group object is extracted, main test is modified

---
+++ group.py
@@ -0,0 +1,8 @@
+__author__ = 'alexei'
+
+
+class Group:
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
--- test_add_group.py
+++ test_add_group.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 import unittest
 
 
@@ -13,22 +14,45 @@ class UntitledTestCase(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_untitled_test_case(self):
+    def test_group_case(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("asdf")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("asdf")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("asdf")
-        driver.find_element_by_name("submit").click()
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="GROUPNAME", header="GROUPHEADER", footer="GROUPFOOTER"))
+        self.return_to_groups_page(driver)
+
+    def test_group_empty(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.return_to_groups_page(driver)
+
+    def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, driver, group):
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_name("submit").click()
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text("GROUPS").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
@@ -46,7 +70,7 @@ class UntitledTestCase(unittest.TestCase):
 
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to.alert()
+            alert = self.driver.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
