0ead23d7166457727f949f721cc954e6df613f43
Sagara <heshagrade@gmail.com>
2018-10-05 21:25:00 +0300

    special helper class of group control is created

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 __author__ = 'Ruslan'
 
@@ -10,24 +11,7 @@ class Application:
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        driver = self.driver
-        self.open_groups_page()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        driver.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("GROUPS").click()
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         driver = self.driver
---
+++ fixture/group.py
@@ -0,0 +1,24 @@
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_groups_page(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("group page").click()
+
+    def create(self, group):
+        driver = self.app.driver
+        self.open_groups_page()
+        driver.find_element_by_name("new").click()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("GROUPS").click()
