9071742e0444cd30ef8cc7031be1ee9269b4ee30
Sagara <heshagrade@gmail.com>
2018-10-10 17:59:49 +0300

    added comments to the application fixture

--- fixture/group.py
+++ fixture/group.py
@@ -12,7 +12,9 @@ class GroupHelper:
     def create(self, group):
         driver = self.app.driver
         self.open_groups_page()
+        # init group creation
         driver.find_element_by_name("new").click()
+        # fill the form
         driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").send_keys(group.footer)
@@ -22,3 +24,12 @@ class GroupHelper:
     def open_groups_page(self):
         driver = self.app.driver
         driver.find_element_by_link_text("GROUPS").click()
+
+    def delete_first_group(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        # select first group
+        driver.find_element_by_name("selected[]").click()
+        # submit deletion
+        driver.find_element_by_name("delete").click()
+        self.return_to_groups_page()
