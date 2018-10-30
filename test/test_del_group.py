9ec5b249fffb2b4b660154e3ecf29f3dce807bec
Sagara <heshagrade@gmail.com>
2018-10-30 18:46:52 +0200

    last update

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,15 +7,19 @@ from model.group import Group
         app.group.delete_first_group()"""
 
 def test_check_all_groups_and_delete(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.delete_all_groups()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+
+    assert 0 == len(new_groups)
 
 def test_delete_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+
+    assert len(old_groups) - 1 == len(new_groups)
 
